6c5bbaf257e4559d8e34cc051f232c050330c716
Matheus Pantoja de Morais <mateus4pantoja@gmail.com>
2024-04-15 21:34:25 -0300

    Integração do banco de dados

---
+++ Loja/src/Classes/usuarioDAO.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class usuarioDAO {
+    
+    public void cadastrarUsuario(usuario Usuario){
+        
+        String sql = "INSERT INTO USARIO(NOME, SENHA) VALUES (?, ?)";
+        
+    }
+
+}
