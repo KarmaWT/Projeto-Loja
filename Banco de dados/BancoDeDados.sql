8936b20b4a0792706c906076b25d0ff6789f46f8
Matheus Pantoja de Morais <mateus4pantoja@gmail.com>
2024-04-18 18:51:16 -0300

    Ajuste no banco de dados

--- Banco de dados/BancoDeDados.sql
+++
@@ -1,9 +0,0 @@
-use mydb;
-
-SELECT * FROM vendedor;
-SELECT * FROM produto;
-
-INSERT INTO `mydb`.`vendedor` (`idvendedor`, `usuario_nome`) VALUES ('1', 'Matheus');
-
-INSERT INTO `mydb`.`PRODUTO` (`idProduto`, `nome`, `preco`, `estoque`, `cod. fabricante`, `vendedor_idvendedor`, `cliente_idcliente`) VALUES ('1', 'camisa', '60', '2', '321', '1', '1');
-INSERT INTO `mydb`.`PRODUTO` (`idProduto`, `nome`, `preco`, `estoque`, `cod. fabricante`, `vendedor_idvendedor`, `cliente_idcliente`) VALUES ('2', 'calça', '50', '5', '123', '1', '1');
