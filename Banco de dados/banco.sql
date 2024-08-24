d94d7edaecdb70887cc72509693787aa4d144a1a
Matheus Pantoja De Morais <mateus4pantoja@gmail.com>
2024-08-24 04:47:11 -0300

    Enviando atualização do Banco de dados e criação da nova tabela.

--- Banco de dados/banco.sql
+++ Banco de dados/banco.sql
@@ -75,8 +75,41 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (1,'Matheus','senha123','04040160223','69992362486','76861000','RO','Itapuã do Oeste','Centro','R. Senado Olavo Pires','1761'),(27,'Bia','napolitano','01010101010','','90810450','RS','Porto Alegre','Santa Tereza','Rua Pinheiro',''),(28,'Resky','1234','02020202020','','76820441','RO','Porto Velho','Flodoaldo Pontes Pinto','Avenida Calama',''),(29,'Joséfina','claudinhobochecha','15646515615','','27250220','RJ','Volta Redonda','Água Limpa','Rua Rio Tocantins','5645'),(30,'Nanah','2788','52197602896','','07097320',NULL,'Guarulhos','Jardim Santa Mena','Rua Dom Macedo da Costa',''),(31,'Klayton','aspas','40022128824','','37570000',NULL,'Ouro Fino','','',''),(34,'Teste','Teste','69999999999','69992282208','76861000','RO','Itapuã do Oeste','Teste','Teste','1234'),(35,'roberto carlos','musica','96666666666','','76861000',NULL,'Itapuã do Oeste','','','');
+INSERT INTO `cliente` VALUES (1,'Matheus','senha','04040160223','69992362486','76861000','RO','Itapuã do Oeste','Centro','R. Senado Olavo Pires','1761'),(27,'Bia','napolitano','01010101010','','90810450','RS','Porto Alegre','Santa Tereza','Rua Pinheiro',''),(28,'Resky','1234','02020202020','','76820441','RO','Porto Velho','Flodoaldo Pontes Pinto','Avenida Calama',''),(29,'Joséfina','claudinhobochecha','15646515615','','27250220','RJ','Volta Redonda','Água Limpa','Rua Rio Tocantins','5645'),(30,'Nanah','2788','52197602896','','07097320',NULL,'Guarulhos','Jardim Santa Mena','Rua Dom Macedo da Costa',''),(31,'Klayton','aspas','40022128824','','37570000',NULL,'Ouro Fino','','',''),(34,'Teste','Teste','69999999999','69992282208','76861000','RO','Itapuã do Oeste','Teste','Teste','1234'),(35,'roberto carlos','musica','96666666666','','76861000',NULL,'Itapuã do Oeste','','','');
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `compra`
+--
+
+DROP TABLE IF EXISTS `compra`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `compra` (
+  `idCompra` int NOT NULL AUTO_INCREMENT,
+  `idCliente` int NOT NULL,
+  `idProduto` int NOT NULL,
+  `quantidade` int NOT NULL,
+  `precoUnitario` decimal(10,2) NOT NULL,
+  `valorTotal` decimal(10,2) NOT NULL,
+  `dataHora` datetime NOT NULL,
+  PRIMARY KEY (`idCompra`),
+  KEY `idCliente` (`idCliente`),
+  KEY `idProduto` (`idProduto`),
+  CONSTRAINT `compra_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idcliente`),
+  CONSTRAINT `compra_ibfk_2` FOREIGN KEY (`idProduto`) REFERENCES `produto` (`idproduto`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `compra`
+--
+
+LOCK TABLES `compra` WRITE;
+/*!40000 ALTER TABLE `compra` DISABLE KEYS */;
+INSERT INTO `compra` VALUES (1,1,4,5,12.00,60.00,'2024-08-23 23:00:37'),(2,1,4,3,12.00,36.00,'2024-08-23 23:13:08'),(3,1,4,1,12.00,12.00,'2024-08-24 01:38:30'),(4,1,4,1,12.00,12.00,'2024-08-24 01:38:40');
+/*!40000 ALTER TABLE `compra` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -103,7 +136,7 @@ CREATE TABLE `produto` (
 
 LOCK TABLES `produto` WRITE;
 /*!40000 ALTER TABLE `produto` DISABLE KEYS */;
-INSERT INTO `produto` VALUES (1,'Camisas',60.00,5,'Camisas simples de  uma cor'),(2,'Calças',60.00,10,'Calças Jeans'),(3,'Colgate',7.00,25,'Pasta de dente colgate'),(4,'Leite',12.00,20,'Leite perfeito para um pai comprar e sumir');
+INSERT INTO `produto` VALUES (1,'Camisas',60.00,5,'Camisas simples de  uma cor'),(2,'Calças',60.00,10,'Calças Jeans'),(3,'Colgate',7.00,25,'Pasta de dente colgate'),(4,'Leite',12.00,15,'Leite perfeito para um pai comprar e sumir');
 /*!40000 ALTER TABLE `produto` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -116,4 +149,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-17  4:44:51
+-- Dump completed on 2024-08-24  4:46:15
